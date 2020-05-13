f9ddf51740a3bcd174d6a003a32dcc74ebff8263
吟阙思町 <68338615@qq.com>
2020-05-13 19:25:15 +0800

    我的

--- jfter/src/router/index.js
+++ jfter/src/router/index.js
@@ -1,16 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Me from '../components/Me.vue'
 
 Vue.use(VueRouter)
 
   const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  }
-]
+    {path: '/', redirect: '/home/me'},
+    {path: '/home', component: Home, children: [
+        {path: '/home/me', component: Me}
+      ]}
+  ]
 
 const router = new VueRouter({
   routes
--- jfter/src/views/Home.vue
+++ jfter/src/views/Home.vue
@@ -1,208 +1,43 @@
 <template>
   <div class="main">
-    <div class="top">
-      <div class="top-top">
-        <div class="top-top-left">
-          <div class="top-top-left-left">
-            <img src="https://s1.ax1x.com/2020/04/22/JUmPuq.png" />
-          </div>
-          <div class="top-top-left-right">
-            <div class="top-top-left-right-top">吟阙思町</div>
-            <div class="top-top-left-right-bottom">ID: yinquesiting408</div>
-          </div>
-        </div>
-        <div class="top-top-right">
-          <i class="iconfont icon-youjiantou" />
-        </div>
-      </div>
-      <div class="top-bottom">
-        <div class="top-bottom-item">
-          <div class="top-bottom-item-top">26</div>
-          <div class="top-bottom-item-bottom">关注</div>
-        </div>
-        <div class="top-bottom-item">
-          <div class="top-bottom-item-top">0</div>
-          <div class="top-bottom-item-bottom">粉丝</div>
-        </div>
-        <div class="top-bottom-item">
-          <div class="top-bottom-item-top">0</div>
-          <div class="top-bottom-item-bottom">推荐</div>
-        </div>
-      </div>
+    <div class="content">
+      <router-view />
     </div>
-    <div class="middle">
-      <div class="middle-item">
-        <div class="middle-item-top"><i class="iconfont icon-icon_fuben" /></div>
-        <div class="middle-item-bottom">收到的喜欢</div>
-      </div>
-      <div class="middle-item">
-        <div class="middle-item-top"><i class="iconfont icon-pinglun" /></div>
-        <div class="middle-item-bottom">评论</div>
-      </div>
-      <div class="middle-item">
-        <div class="middle-item-top"><i class="iconfont icon-qunliao" /></div>
-        <div class="middle-item-bottom">聊天</div>
-      </div>
-      <div class="middle-item">
-        <div class="middle-item-top"><i class="iconfont icon-tongzhi" /></div>
-        <div class="middle-item-bottom">通知</div>
-      </div>
-    </div>
-    <div class="bottom-line">
-      <div class="item">
-        <div class="item-left">
-          <i class="iconfont icon-icon-test" />
+    <div class="bottom">
+      <div class="bottom-item">
+        <div class="bottom-item-top">
+          <i class="iconfont icon-shouye" />
         </div>
-        <div class="item-right">
-          <div class="item-right-left">
-            我的收藏
-          </div>
-          <div class="item-right-right">
-            <i class="iconfont icon-youjiantou" />
-          </div>
+        <div class="bottom-item-bottom">
+          首页
         </div>
       </div>
-      <div class="item">
-        <div class="item-left">
-          <i class="iconfont icon-icon_fuben" />
+      <div class="bottom-item">
+        <div class="bottom-item-top">
+          <i class="iconfont icon-faxian" />
         </div>
-        <div class="item-right">
-          <div class="item-right-left">
-            我的喜欢
-          </div>
-          <div class="item-right-right">
-            <i class="iconfont icon-youjiantou" />
-          </div>
+        <div class="bottom-item-bottom">
+          发现
         </div>
       </div>
-      <div class="item">
-        <div class="item-left">
-          <i class="iconfont icon-wallet_icon" />
+      <div class="bottom-more">
+        <i class="iconfont icon-jiahao" />
+      </div>
+      <div class="bottom-item">
+        <div class="bottom-item-top">
+          <i class="iconfont icon-shangpin" />
         </div>
-        <div class="item-right">
-          <div class="item-right-left">
-            我的钱包
-          </div>
-          <div class="item-right-right">
-            <i class="iconfont icon-youjiantou" />
-          </div>
+        <div class="bottom-item-bottom">
+          市集
         </div>
       </div>
-      <div class="item">
-        <div class="item-left">
-          <i class="iconfont icon-zuji" />
+      <div class="bottom-item">
+        <div class="bottom-item-top">
+          <i class="iconfont icon-yonghu" />
         </div>
-        <div class="item-right">
-          <div class="item-right-left">
-            我的足迹
-          </div>
-          <div class="item-right-right">
-            <i class="iconfont icon-youjiantou" />
-          </div>
+        <div class="bottom-item-bottom">
+          我的
         </div>
-      </div>
-      <div class="item">
-        <div class="item-left">
-          <i class="iconfont icon-youxiang" />
-        </div>
-        <div class="item-right">
-          <div class="item-right-left">
-            我的提问箱
-          </div>
-          <div class="item-right-right">
-            <i class="iconfont icon-youjiantou" />
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="bottom-line">
-      <div class="item">
-        <div class="item-left">
-          <i class="iconfont icon-V" />
-        </div>
-        <div class="item-right">
-          <div class="item-right-left">
-            达人认证
-          </div>
-          <div class="item-right-right">
-            <i class="iconfont icon-youjiantou" />
-          </div>
-        </div>
-      </div>
-      <div class="item">
-        <div class="item-left">
-          <i class="iconfont icon-J" />
-        </div>
-        <div class="item-right">
-          <div class="item-right-left">
-            创作者签约
-          </div>
-          <div class="item-right-left">
-            <i class="iconfont icon-youjiantou" />
-          </div>
-        </div>
-      </div>
-      <div class="item">
-        <div class="item-left">
-          <i class="iconfont icon-u" />
-        </div>
-        <div class="item-right">
-          <div class="item-right-left">
-            个人应用
-          </div>
-          <div class="item-right-right">
-            <i class="iconfont icon-youjiantou" />
-          </div>
-        </div>
-      </div>
-      <div class="item">
-        <div class="item-left">
-          <i class="iconfont icon-duxiaoshuo" />
-        </div>
-        <div class="item-right">
-          <div class="item-right-left">
-            看小说
-          </div>
-          <div class="item-right-right">
-            <i class="iconfont icon-youjiantou" />
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="bottom-line">
-      <div class="item">
-        <div class="item-left">
-          <i class="iconfont icon-erji" />
-        </div>
-        <div class="item-right">
-          <div class="item-right-left">
-            帮助与反馈
-          </div>
-          <div class="item-right-right">
-            <i class="iconfont icon-youjiantou" />
-          </div>
-        </div>
-      </div>
-      <div class="item">
-        <div class="item-left">
-          <i class="iconfont icon-icon_setting" />
-        </div>
-        <div class="item-right">
-          <div class="item-right-left">
-            设置
-          </div>
-          <div class="item-right-right">
-            <i class="iconfont icon-youjiantou" />
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="advertisement">
-      <div class="advertisement-img">
-        <img src="https://s1.ax1x.com/2020/04/24/JB6bf1.png" />
-      </div>
-      <div class="advertisement-text">
-        <span>广告</span>
       </div>
     </div>
   </div>
@@ -213,168 +48,41 @@
 </script>
 <style lang="less" scoped>
 .main {
-  background-color: #eee;
-  .top {
+  .content {
+    height: 92vh;
     width: 100vw;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    .top-top {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      width: 100%;
-      box-sizing: border-box;
-      padding: 5vw 5vw;
-      .top-top-left {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        .top-top-left-left {
-          margin-right: 4vw;
-          img {
-            width: 20vw;
-            border-radius: 50%;
-          }
-        }
-        .top-top-left-right {
-          .top-top-left-right-top {
-            font-size: 5vw;
-            font-weight: bold;
-            margin-bottom: 2vw;
-          }
-          .top-top-left-right-bottom {
-            font-size: 3vw;
-            color: #555;
-          }
-        }
-      }
-      .top-top-right {
-        i {
-          font-size: 5vw;
-        }
-      }
-    }
-    .top-bottom {
-      margin: 3vw 0;
-      display: flex;
-      justify-content: space-around;
-      align-items: center;
-      width: 100vw;
-      .top-bottom-item {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-around;
-        align-items: center;
-        .top-bottom-item-top {
-          margin-bottom: 1vw;
-          font-size: 4vw;
-        }
-        .top-bottom-item-bottom {
-          font-size: 3vw;
-          color: #555;
-        }
-      }
-    }
+    overflow: scroll;
   }
-  .middle {
-    background-color: #fff;
+  .bottom {
+    height: 8vh;
+    width: 100vw;
     display: flex;
     justify-content: space-around;
     align-items: center;
-    width: 95vw;
-    margin: 2vw auto;
-    border-radius: 2vw;
-    .middle-item {
+    .bottom-item {
       display: flex;
       flex-direction: column;
-      justify-content: space-around;
+      justify-content: space-between;
       align-items: center;
-      .middle-item-top {
-        background-color: aqua;
-        border-radius: 50%;
-        padding: 2vw;
-        margin: 3vw 0;
-        i {
-          font-size: 7vw;
-          color: #fff;
-        }
-      }
-      .middle-item-bottom {
-        font-size: 3vw;
-        margin: 0 0 3vw 0;
-      }
-    }
-    .middle-item:nth-child(1) {
-      .middle-item-top {
-        background-color: pink;
-      }
-    }
-    .middle-item:nth-child(2) {
-      .middle-item-top {
-        background-color: cornflowerblue;
-      }
-    }
-    .middle-item:nth-child(3) {
-      .middle-item-top {
-        background-color: mediumpurple;
-      }
-    }
-    .middle-item:nth-child(4) {
-      .middle-item-top {
-        background-color: darkorange;
-      }
-    }
-  }
-  .bottom-line {
-    border: solid 0.1vw #fff;
-    width: 95vw;
-    background-color: white;
-    margin: 3vw auto 0;
-    border-radius: 2vw;
-    .item {
-      margin-top: 3vw;
-      display: flex;
-      justify-content: start;
-      align-items: center;
-      .item-left {
-        margin: 0 3vw;
+      height: 6vh;
+      .bottom-item-top {
         i {
           font-size: 6vw;
         }
       }
-      .item-right {
-        width: 80vw;
-        padding: 3vw 0;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        border-bottom: #eee solid 0.1vw;
-        .item-right-left {
-          font-size: 4.5vw;
-        }
-        .item-right-right {
-
-        }
+      .bottom-item-bottom {
+        font-size: 3vw;
       }
     }
-  }
-  .advertisement {
-    .advertisement-img {
-      margin: 2vw auto;
-      background-color: #42b983;
-      width: 95vw;
-      border-radius: 2vw;
-      overflow: hidden;
-      img {
-        width: 95vw;
-        height: auto;
+    .bottom-more {
+      padding: 2vw;
+      border-radius: 50%;
+      background-image: linear-gradient(to bottom right, darkcyan, cyan);
+      i {
+        font-weight: bold;
+        font-size: 4vw;
+        color: white;
       }
-    }
-    .advertisement-text {
-      color: #fff;
-      font-size: 3vw;
     }
   }
 }
